ef9023f627aca6c715df16e728b4a1c689a72a5b
ChrisBall <chrisballprojects@gmail.com>
2017-10-09 18:10:15 +0100

    DFPlayer reference functions
    play, loop, stop & volume functions

---
+++ microbit_scripts/dfplayer_functions.py
@@ -0,0 +1,33 @@
+#recommended to write pin16 low (this is the busy pin indicator)
+
+Start_Byte = 0x7E
+Version_Byte = 0xFF
+CMD_Length = 0x06
+Acknowledge = 0x00
+End_Byte = 0xEF
+HighByte = 0x00
+LowByte = 0x00
+
+def split(num):
+    return num >> 8, num & 0xFF
+
+def command(CMD, Par1, Par2):
+    Checksum = -(Version_Byte + CMD_Length + CMD + Acknowledge + Par1 + Par2)
+    HighByte, LowByte = split(Checksum)
+    CommandLine = bytes([b & 0xFF for b in [
+        Start_Byte, Version_Byte, CMD_Length, CMD, Acknowledge,
+        Par1, Par2, HighByte, LowByte, End_Byte
+    ]])
+    uart.write(CommandLine)
+
+#folders named "##", e.g. "00" to "99" with tracks named "###.mp3", e.g. "000.mp3" to "255.mp3"
+def playTrack(Folder,Track):
+    command(0x0F,int(Folder),int(Track))  
+
+#stops any track playing    
+def stopTrack():
+    command(0x16,0,0)
+    
+#volume should be int between 0 and 30
+def setVolume(Volume):
+    command(0x6,0,int(Volume))
